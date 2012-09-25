36083e16a38ec3842db41ef5f5afb29c6d111a5d
Brandon Lamb <brandon@brandonlamb.com>
2012-09-24 21:29:51 -0700

    More formatting

--- example.php
+++ example.php
@@ -3,22 +3,20 @@ require 'Router.php';
 require 'Route.php';
 
 $router = new Router();
-
 $router->setBasePath('/PHP-Router');
-
 $router->map('/', 'someController:indexAction', array('methods' => 'GET'));
-$router->map('/users/','users#create', array('methods' => 'POST', 'name' => 'users_create'));
+$router->map('/users/', 'users#create', array('methods' => 'POST', 'name' => 'users_create'));
 $router->map('/users/:id/edit/', 'users#edit', array('methods' => 'GET', 'name' => 'users_edit', 'filters' => array('id' => '(\d+)')));
-$router->map('/contact/',array('controller' => 'someController', 'action' => 'contactAction'), array('name' => 'contact'));
-
+$router->map('/contact/', array('controller' => 'someController', 'action' => 'contactAction'), array('name' => 'contact'));
 $router->map('/blog/:slug', array('c' => 'BlogController', 'a' => 'showAction'));
 
 // capture rest of URL in "path" parameter (including forward slashes)
-$router->map('/site-section/:path','some#target',array( 'filters' => array( 'path' => '(.*)') ) );
+$router->map('/site-section/:path', 'some#target', array('filters' => array('path' => '(.*)')));
 
 $route = $router->matchCurrentRequest();
+?>
 
-?><h3>Current URL & HTTP method would route to: </h3>
+<h3>Current URL & HTTP method would route to: </h3>
 <?php if($route) { ?>
 	<strong>Target:</strong>
 	<pre><?php var_dump($route->getTarget()); ?></pre>
